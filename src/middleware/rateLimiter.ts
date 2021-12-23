b453584bc7a5ccda758150fe6bd37727c37fbb7e
afif1400 <afifahmed456123@gmail.com>
2021-12-24 00:34:50 +0530

    🐛 removed bug in rate limiter

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -33,51 +33,51 @@ export const rateLimiter = async (
     newRecord.push(requestLog);
     client.set(ip, JSON.stringify(newRecord));
     next();
-  }
-
-  let data = JSON.parse(record!);
-  let windowStartTimeStamp = moment().subtract(WINDOW_SIZE, "hours").unix();
-  let requestWithinWindow = data.filter((requestEntry: IRequestLog) => {
-    return requestEntry.requestTimeStamp > windowStartTimeStamp;
-  });
-
-  let totalWindowRequestCount = requestWithinWindow.reduce(
-    (accumulator: any, entry: IRequestLog) => {
-      return accumulator + entry.requestCount;
-    },
-    0
-  );
-
-  // If number of requests made is greater than or equal to the desired maximum, return error
-  if (totalWindowRequestCount >= MAX_WINDOW_REQUEST) {
-    res
-      .status(429)
-      .send(
-        new CustomError(
-          429,
-          `You hav exceeded ${MAX_WINDOW_REQUEST} request in ${WINDOW_SIZE} hours limit`
-        )
-      );
   } else {
-    // If number of requests made is less that maximum, log the new request recoed
-    let lastRequestLog: IRequestLog = data[data.length - 1];
-    let currentWindowIntervalStartTimeStamp: number = currentRequestTime
-      .subtract(WINDOW_LOG_INTERVAL, "hours")
-      .unix();
+    let data = JSON.parse(record!);
+    let windowStartTimeStamp = moment().subtract(WINDOW_SIZE, "hours").unix();
+    let requestWithinWindow = data.filter((requestEntry: IRequestLog) => {
+      return requestEntry.requestTimeStamp > windowStartTimeStamp;
+    });
 
-    // If interval has not passed since last request log, increment counter
-    if (lastRequestLog.requestCount > currentWindowIntervalStartTimeStamp) {
-      lastRequestLog.requestTimeStamp++;
-      data[data.length - 1] = lastRequestLog;
+    let totalWindowRequestCount = requestWithinWindow.reduce(
+      (accumulator: any, entry: IRequestLog) => {
+        return accumulator + entry.requestCount;
+      },
+      0
+    );
+
+    // If number of requests made is greater than or equal to the desired maximum, return error
+    if (totalWindowRequestCount >= MAX_WINDOW_REQUEST) {
+      res
+        .status(429)
+        .send(
+          new CustomError(
+            429,
+            `You hav exceeded ${MAX_WINDOW_REQUEST} request in ${WINDOW_SIZE} hours limit`
+          )
+        );
     } else {
-      // If interval has passed, log new entry for current user and timestamp
-      data.push({
-        requestTimeStamp: currentRequestTime.unix(),
-        requestCount: 1,
-      });
-    }
+      // If number of requests made is less that maximum, log the new request recoed
+      let lastRequestLog: IRequestLog = data[data.length - 1];
+      let currentWindowIntervalStartTimeStamp: number = currentRequestTime
+        .subtract(WINDOW_LOG_INTERVAL, "hours")
+        .unix();
 
-    client.set(ip, JSON.stringify(data));
-    next();
+      // If interval has not passed since last request log, increment counter
+      if (lastRequestLog.requestCount > currentWindowIntervalStartTimeStamp) {
+        lastRequestLog.requestTimeStamp++;
+        data[data.length - 1] = lastRequestLog;
+      } else {
+        // If interval has passed, log new entry for current user and timestamp
+        data.push({
+          requestTimeStamp: currentRequestTime.unix(),
+          requestCount: 1,
+        });
+      }
+
+      client.set(ip, JSON.stringify(data));
+      next();
+    }
   }
 };
