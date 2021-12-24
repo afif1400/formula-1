a2c51dce2dad594fa89c22e29dfef85bff678a64
afif1400 <afifahmed456123@gmail.com>
2021-12-24 11:41:54 +0530

    🐛 added caching for circuits get

--- src/routes/circuit/get.ts
+++ src/routes/circuit/get.ts
@@ -4,15 +4,19 @@ import { Circuit } from "../../models/circuits";
 
 const router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
-  Circuit.find({}, (err, circuits) => {
-    if (err) {
-      return res.status(400).send(new CustomError(400, err.message));
-    }
-    return res.status(200).send({
-      data: circuits,
-    });
+router.get("/", async (req: Request, res: Response) => {
+  //@ts-ignore
+  const circuits = await Circuit.find({}).cache({
+    originalUrl: req.originalUrl,
+    key: req.ip,
   });
+  if (!circuits) {
+    return res
+      .status(400)
+      .send(new CustomError(400, "Unable to fetch circuits from database"));
+  }
+
+  return res.status(200).send({ data: circuits });
 });
 
 export { router as circuitGetRouter };
